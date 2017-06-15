57892860481eab0c4be6b7ac654b772762564f29
Shashank Teotia <shashankteotia@gmail.com>
2017-06-15 16:16:29 +0530

    moving to ash from bash

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 # Build script
 # set -o errexit
 e () {
--- src/metadata.sh
+++ src/metadata.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 # return version
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cat ${parent_path}/package.json| jq '{name: .name , version: .version, author: .author}'
