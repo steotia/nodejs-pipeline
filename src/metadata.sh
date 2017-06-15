be96c7c7cce2af4c840bf312092a9f7e6787ecea
Shashank Teotia <shashankteotia@gmail.com>
2017-06-15 17:04:34 +0530

    correction

--- src/metadata.sh
+++ src/metadata.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # return version
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+parent_path=$( cd "$(dirname "$0")" ; pwd -P )
 cat ${parent_path}/package.json| jq '{name: .name , version: .version, author: .author}'
