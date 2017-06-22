bf3cc9312e925ca861d721c1dc53936833831488
Shashank Teotia <shashankteotia@gmail.com>
2017-06-22 09:19:46 +0530

    ssh changes

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,4 @@ echo "ARTIFACT_LABEL: ${artifactLabel}"
 
 REPLICAS=${replicas} ENV=${env} AUTHOR=${author} NAME=${name} \
 TAG=${version}-${artifactLabel} \
-ssh -A -i /run/secrets/read-key read@${MANAGER_IP} "docker stack deploy -c docker-compose.yml ${name}-${env}"
+ssh -o StrictHostKeyChecking=no -i /run/secrets/read-key read@${MANAGER_IP} "docker stack deploy -c docker-compose.yml ${name}-${env}"
